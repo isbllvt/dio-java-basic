8216fd80fdfcf438d59294ccd60ad00426b2ff1f
Isabelle Vitoria <isav.mendonca@gmail.com>
2024-05-22 21:30:58 -0300

    methodsTest

---
+++ anatomia-metodos-e-variaveis/src/MinhaClass.java
@@ -0,0 +1,16 @@
+public class MinhaClass {
+
+    public static void main(String [] args) {
+        String firstName = "Isabelle";
+        String lastName = "Santiago";
+
+        String completeName = completeName(firstName, lastName);
+        System.out.println(completeName);
+}
+
+        public static String completeName (String firstName, String lastName){
+            return "methods result: " + firstName.concat(" ").concat(lastName);
+
+}
+
+}
